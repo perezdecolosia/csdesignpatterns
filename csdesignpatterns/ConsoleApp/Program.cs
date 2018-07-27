14862b7a2aa42697769fa7bf92906b068f6eceb9
Jose Manuel Gil <jmgil@enac.es>
2018-07-27 14:02:52 +0200

    Fist commit. Added until chapter6

---
+++ csdesignpatterns/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Chapter01.Client();
+            c1.Main();
+        }
+
+        private void Run()
+        {
+          
+        }
+    }
+}
